f4d11a686234531e9ab05c65a858568b2f4390f5
Kev <Kevin_Heitz_student2019@wilder.school>
2019-04-18 22:45:23 +0200

    starting Controller

--- src/Controller/StartingController.php
+++ src/Controller/StartingController.php
@@ -8,56 +8,40 @@
 
 namespace App\Controller;
 
-use GuzzleHttp\Exception\GuzzleException;
-use Twig_Error_Loader;
-use Twig_Error_Runtime;
-use Twig_Error_Syntax;
-
 class StartingController extends GuzzleController
 {
-    /**
-     * @return string
-     * @throws GuzzleException
-     * @throws Twig_Error_Loader
-     * @throws Twig_Error_Runtime
-     * @throws Twig_Error_Syntax
-     */
     public function chooseCharacter()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['username'])) {
+                // Tape un username
+                $_SESSION['username1']=$_POST['username1'];
+                $_SESSION['username2']=$_POST['username2'];
+            }
+                // Recupère l'id personnage
+            $_SESSION['userId1'] = $_POST['userId1'];
+            $_SESSION['userId2'] = $_POST['userId2'];
+        }
         // Affiche 4 cartes de personnages aléatoire pour le joueur 1
-
-
+        $list1 = [];
         $idList1 = [];
-        while (count($idList1 < 4)) {
+        while (count($idList1) < 4) {
             $character1 = $this->characterRandom();
             if (!in_array($character1['id'], $idList1)) {
                 $idList1[] = $character1['id'];
                 $list1[] = $character1;
-
             }
         }
-
-        //joueur 1 en choisit 1
-
-
         // Affiche 4 cartes de personnages aléatoire pour le joueur 2
-
+        $list2 = [];
         $idList2 = [];
-        while (count($idList2 < 4)) {
+        while (count($idList2) < 4) {
             $character2 = $this->characterRandom();
             if (!in_array($character2['id'], $idList2)) {
                 $idList2[] = $character2['id'];
                 $list2[] = $character2;
             }
         }
-        //joueur 2 en choisit 1
-
-
-        //La partie commence
-        // Le compteur de chaque joueur s'initialise
-        $compteur1 = 0;
-        $compteur2 = 0;
-
-        return $this->twig->render('Item/index.html.twig', ['characters1' => $list1], ['characters2' => $list2]);
+        return $this->twig->render('Home/form.html.twig', ['characters1' => $list1, 'characters2' => $list2]);
     }
 }
