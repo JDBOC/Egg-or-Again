3279ef8eb90ef6477c2a18ac7567f50581a8cb86
Kev <Kevin_Heitz_student2019@wilder.school>
2019-04-19 11:15:27 +0200

    win win

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -49,9 +49,9 @@ class GameController extends GuzzleController
                                                     :$_SESSION['player'] = 'player_1';
                     // Initialisation compteurs perdus et incrémentation à chaque Junk
                     if ($_SESSION['player'] == "player_1") {
-                        $_SESSION['perdu1'] =+ 1;
+                        $_SESSION['perdu1'] += 1;
                     } else {
-                        $_SESSION['perdu2'] =+ 1;
+                        $_SESSION['perdu2'] += 1;
                     }
                 } else {//sinon
                     // ajoute la valeur au compteur secondaire
@@ -62,9 +62,9 @@ class GameController extends GuzzleController
 
                 // Initialisation compteurs roll et incrémentation à chaque roll
                 if ($_SESSION['player'] == "player_1") {
-                    $_SESSION['roll1'] =+ 1;
+                    $_SESSION['roll1'] += 1;
                 } else {
-                    $_SESSION['roll2'] =+ 1;
+                    $_SESSION['roll2'] += 1;
                 }
 
                 return $this->twig->render('Home/game.html.twig', ['session' => $_SESSION,
@@ -81,9 +81,9 @@ class GameController extends GuzzleController
 
                 // Initialisation compteurs hold et incrémentation à chaque hold
                 if ($_SESSION['player'] == "player_1") {
-                    $_SESSION['hold1'] =+ 1;
+                    $_SESSION['hold1'] += 1;
                 } else {
-                    $_SESSION['hold2'] =+ 1;
+                    $_SESSION['hold2'] += 1;
                 }
 
                 //Vérifie si le compteur principal atteint 100
